4eb930a7700c9316f3f444d8f0ec250abdba2ac5
Luan Santos <luandunaas@gmail.com>
2023-03-13 04:16:03 -0300

    Adicionado material fonts ao projeto
    feat: foi adicionado Material Fonts ao projeto para a utilização de icones personalizados. Alterado o texto de Show/Hide password para funcionar junto ao Material Fonts

--- web/src/components/Login.tsx
+++ web/src/components/Login.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import '../css/Login.css';
 import jettImg from '../img/jett.png';
 import vcl_logo from '../img/vcl_logo.png';
-import {useState} from 'react';
+import { useState } from 'react';
 
 let Login = () => {
     let msg = "";
@@ -16,10 +16,10 @@ let Login = () => {
     }
 
     const validateEmail = () => {
-        if(!emails.includes(loginDefault)){
+        if (!emails.includes(loginDefault)) {
             msg += "Login inválido! \n"
         }
-        if(msg){
+        if (msg) {
             alert(msg);
         }
     }
@@ -34,22 +34,22 @@ let Login = () => {
                 <div className='form'>
                     <div className='login-form-input'>
                         <label htmlFor="username">Email</label>
-                        <input type="text" name="username" id="" value={loginDefault} onChange={e => newValueLogin(e.target.value)}/>
+                        <input type="text" name="username" id="" value={loginDefault} onChange={e => newValueLogin(e.target.value)} />
                     </div>
 
                     <div className='login-form-input'>
                         <label htmlFor="password">Password</label>
-                        <input type={showPass ? "text" : "password"} name="password" id="input1" value={passDefault} onChange={e => newValuePass(e.target.value)}/>
-                        <label onClick={viewPassword} className="waves-effect labelPass">
-                            {showPass ? "Hide Password" : "Show Password"}
-                        </label>
+                        <div className='form_input'>
+                            <input type={showPass ? "text" : "password"} name="password" id="inputPassword" value={passDefault} onChange={e => newValuePass(e.target.value)} />
+                            <span className="material-icons md-16" id='pass-visibility-icon' onClick={viewPassword}>{showPass ? "visibility_off" : "visibility"}</span>
+                        </div>
                     </div>
                     <button className='waves-effect waves-light btn' onClick={validateEmail}>Sign In</button>
                 </div>
             </div>
             <div className='right'>
                 <div className='background-right'>
-                    <img src={vcl_logo} alt=""/>
+                    <img src={vcl_logo} alt="" />
                 </div>
             </div>
         </div>
